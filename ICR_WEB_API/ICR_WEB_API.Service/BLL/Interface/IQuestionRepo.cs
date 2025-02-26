ea0cf52c9ec1cb15d71f8a6377a16a9b39821508
HP <shahrairsojib@gmail.com>
2025-02-26 08:12:02 +0600

    repo added

---
+++ ICR_WEB_API/ICR_WEB_API.Service/BLL/Interface/IQuestionRepo.cs
@@ -0,0 +1,14 @@
+﻿using ICR_WEB_API.Service.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICR_WEB_API.Service.BLL.Interface
+{
+    public interface IQuestionRepo 
+    {
+        Task<List<Question>> GetAll();
+    }
+}
---
+++ ICR_WEB_API/ICR_WEB_API.Service/BLL/Repository/QuestionsRepo.cs
@@ -0,0 +1,28 @@
+﻿using ICR_WEB_API.Service.BLL.Interface;
+using ICR_WEB_API.Service.BLL.Services;
+using ICR_WEB_API.Service.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICR_WEB_API.Service.BLL.Repository
+{
+    public  class QuestionsRepo: IQuestionRepo
+    {
+        private readonly ICRSurveyDBContext _surveyDBContext;
+        public QuestionsRepo(ICRSurveyDBContext context)
+        {
+                _surveyDBContext = context;
+        }
+        public async Task<List<Question>> GetAll()
+        {
+            var list = new List<Question>();
+            list = await _surveyDBContext.Questions.ToListAsync();
+            return list;
+        }
+    }
+}
