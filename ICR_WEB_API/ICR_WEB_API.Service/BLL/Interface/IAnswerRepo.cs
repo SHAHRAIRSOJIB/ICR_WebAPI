28939de4268ff0735e7f1395061535073000f322
HP <shahrairsojib@gmail.com>
2025-02-28 20:23:50 +0600

    AnswerDto Added..

--- ICR_WEB_API/ICR_WEB_API.Service/BLL/Interface/IAnswerRepo.cs
+++ ICR_WEB_API/ICR_WEB_API.Service/BLL/Interface/IAnswerRepo.cs
@@ -1,10 +1,11 @@
 ﻿using ICR_WEB_API.Service.Entity;
+using ICR_WEB_API.Service.Model.DTOs;
 
 namespace ICR_WEB_API.Service.BLL.Interface
 {
     public interface IAnswerRepo
     {
-        Task<List<Answer>> GetAll();
+        Task<List<AnswerDTO>> GetAll();
         Task<int> Save(List<Answer> entities);
         Task<string> Update(Answer entity);
     }
---
+++ ICR_WEB_API/ICR_WEB_API.Service/Model/DTOs/AnswerDTO.cs
@@ -0,0 +1,48 @@
+﻿using ICR_WEB_API.Service.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICR_WEB_API.Service.Model.DTOs
+{
+    public class AnswerDTO
+    {
+        public int Id { get; set; }
+
+        public int ResponseId { get; set; }
+        public int QuestionId { get; set; }
+
+        public int? SelectedOptionId { get; set; }
+
+        public int? RatingItemId { get; set; }
+        public int? RatingValue { get; set; } 
+
+        public string? TextResponse { get; set; }
+
+        public  QuestionDTO? Question { get; set; }
+        public  ResponseDTO? Response { get; set; }
+    }
+    public class ResponseDTO {
+        public int Id { get; set; }
+        public DateTime SubmissionDate { get; set; }
+
+        public string ShopName { get; set; }                    // اسم المحل
+        public string OwnerName { get; set; }                   // اسم المالك
+        public string UnifiedLicenseNumber { get; set; }        // رقم الرخصة الموحد
+        public string LicenseIssueDateLabel { get; set; }       // تاريخ اصدار الرخصة
+        public string OwnerIDNumber { get; set; }               // رقم هوية المالك
+        public string AIESECActivity { get; set; }              // نشاط الايزك
+        public string Municipality { get; set; }                // البلدية
+        public string FullAddress { get; set; }                 // العنوان الكامل
+
+        // Navigation Property
+        public int UserId { get; set; }
+    }
+
+   
+
+
+
+}
